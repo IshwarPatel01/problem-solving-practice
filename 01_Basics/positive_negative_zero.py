547946a479429dec457e1494237e9e9b49b9c8ae
Ishwar Patel <IshwarPatel01@users.noreply.github.com>
2025-02-20 12:55:23 +0530

    add problem positive,negative or zero

---
+++ 01_Basics/positive_negative_zero.py
@@ -0,0 +1,14 @@
+# take input integer "n"
+# if n == 0 ,print "Zero"
+# else-if n < 0 , print "negative"
+# else print "Positive"
+
+
+n = int(input())
+
+if n == 0:
+    print("Zero")
+elif n < 0:
+    print("Negative")
+else:
+    print("Positive")
