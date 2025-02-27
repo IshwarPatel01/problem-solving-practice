66d3177eee01c65cc8f4aaafc620e372c7f15b40
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-02-27 20:16:31 +0530

    add fibonacci proble,

--- 01_Basics/fibonacci.py
+++ 01_Basics/fibonacci.py
@@ -7,4 +7,10 @@
 # Output: [0, 1, 1, 2, 3, 5, 8]
 # Explanation: The Fibonacci series up to 10 is generated as [0, 1, 1, 2, 3, 5, 8].
 
+n= 10
 
+fib = [0,1]
+
+while fib[-1] + fib[-2] < n:
+    fib.append(fib[-1] + fib[-2])
+print(fib)
