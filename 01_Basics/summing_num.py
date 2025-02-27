3cd96df779bf1de05f496e031a76072dc31d6e0c
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-02-27 20:01:09 +0530

    add summing problem

--- 01_Basics/summing_num.py
+++ 01_Basics/summing_num.py
@@ -7,3 +7,13 @@
 # Output: 10
 # Explanation: The sum of the digits 1 + 2 + 3 + 4 = 10.
 
+n = 1234
+total = 0
+temp = n
+
+while temp > 0:
+    digit =  temp % 10
+    total += digit
+    temp //= 10
+
+print(total)
