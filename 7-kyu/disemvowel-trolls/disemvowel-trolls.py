07326ca3385150d0cda8720a64b3c46511ae8a57
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-05-13 17:25:15 +0530

    Add solution - CodeHub

---
+++ 7-kyu/disemvowel-trolls/disemvowel-trolls.py
@@ -0,0 +1,10 @@
+def disemvowel(string_):
+    vowels_list = ["a","A","E","e","i","I","o","O","u","U"]
+#     vowels_list = ["a","e","i","o","u"]    
+    result = ""
+    for char in string_:
+        if char not in vowels_list:
+            result += char
+        
+    
+    return result
