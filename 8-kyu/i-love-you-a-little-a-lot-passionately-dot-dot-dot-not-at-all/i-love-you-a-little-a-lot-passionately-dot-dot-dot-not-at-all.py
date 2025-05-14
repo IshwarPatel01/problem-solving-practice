13381eab96718956c316185cadf342c9616f4c17
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-05-14 18:24:16 +0530

    Add solution - CodeHub

---
+++ 8-kyu/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all.py
@@ -0,0 +1,7 @@
+def how_much_i_love_you(nb_petals):
+    # your code
+    n = nb_petals % 6
+    
+    arr = ["not at all" ,"I love you" ,"a little", "a lot", "passionately", "madly"]
+    
+    return arr[n]
