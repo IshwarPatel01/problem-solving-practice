5c00a643683842ccf99d5dcf0ea114287f6264d5
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-05-14 12:30:59 +0530

    Update solution - CodeHub

--- 8-kyu/basic-mathematical-operations/basic-mathematical-operations.py
+++ 8-kyu/basic-mathematical-operations/basic-mathematical-operations.py
@@ -1,10 +1,41 @@
-def basic_op(operator, value1, value2):
-    #your code here
-    if operator == "+":
-        return value1 + value2
-    elif operator == "-":
-        return value1 - value2
-    elif operator == "*":
-        return value1 * value2
-    else:
+ 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         return value1 / value2
