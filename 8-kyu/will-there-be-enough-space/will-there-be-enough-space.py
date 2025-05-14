3b8fd88c38e47124fce5d5acb0d2653aad642d4e
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-05-14 12:49:52 +0530

    Update solution - CodeHub

--- 8-kyu/will-there-be-enough-space/will-there-be-enough-space.py
+++ 8-kyu/will-there-be-enough-space/will-there-be-enough-space.py
@@ -1,9 +1,9 @@
+ 
 def enough(cap, on, wait):
     # Your code here
-    
     total_passenger = on + wait
     if cap >= total_passenger:
+​
         return 0
     else:
         return abs(cap - total_passenger)
-    
