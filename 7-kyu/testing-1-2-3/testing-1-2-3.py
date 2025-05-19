eddeeb1dd1ce66ab729a5d37ed0a02f38566052c
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-05-19 13:18:21 +0530

    Add solution - CodeHub

---
+++ 7-kyu/testing-1-2-3/testing-1-2-3.py
@@ -0,0 +1,12 @@
+ 
+def number(lines):
+    #your code here
+    n = 1
+    arr = []
+    if len(lines) == 0:
+        return []
+    for i in lines:
+        result = f"{n}: {i}"
+        arr.append(result)
+        n += 1
+    return arr
