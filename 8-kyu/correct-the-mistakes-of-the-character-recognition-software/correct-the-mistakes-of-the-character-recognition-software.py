69ee19cb251e5a3ecceb5f5e5046d16974b67790
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-05-19 17:42:22 +0530

    Add solution - CodeHub

---
+++ 8-kyu/correct-the-mistakes-of-the-character-recognition-software/correct-the-mistakes-of-the-character-recognition-software.py
@@ -0,0 +1,14 @@
+def correct(s):
+    
+#     mistakes = [5,0,1]
+    
+#     for char in s:
+#         if char == 5:
+    s = s.replace("5","S")
+#         elif char == 0:
+    s = s.replace("0","O")
+#         elif char == 1:
+    s = s.replace("1","I")
+    return s
+            
+        
