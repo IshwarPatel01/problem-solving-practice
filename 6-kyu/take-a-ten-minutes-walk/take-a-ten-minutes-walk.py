4a37b56bdfc62b7ecd6570c5b1c1164b7d3ca722
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-05-26 14:36:16 +0530

    Update solution - CodeHub

--- 6-kyu/take-a-ten-minutes-walk/take-a-ten-minutes-walk.py
+++ 6-kyu/take-a-ten-minutes-walk/take-a-ten-minutes-walk.py
@@ -1,9 +1,8 @@
- 
 def is_valid_walk(walk):
     #determine if walk is valid
     return (
         len(walk) == 10 and
         walk.count("n") == walk.count("s") and
         walk.count("e") == walk.count("w")
-        
     )
+        
