3bfe1dc8adbc557c978bed1c330bc19ac6d69e7b
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-05-26 14:50:01 +0530

    Add solution - CodeHub

---
+++ 8-kyu/find-the-first-non-consecutive-number/find-the-first-non-consecutive-number.py
@@ -0,0 +1,25 @@
+def first_non_consecutive(arr):
+    n = len(arr)
+    
+    for i in range(1, n):
+        if arr[i] != arr[i - 1] + 1:
+            return arr[i]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
