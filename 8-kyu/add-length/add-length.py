b7b0f3e9877e192195d19188e063f73ccd1e7512
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-06-10 14:40:33 +0530

    Add solution - CodeHub

---
+++ 8-kyu/add-length/add-length.py
@@ -0,0 +1,10 @@
+ 
+def add_length(str_):
+    arr = []
+    arr2 = str_.split()
+    result = ""
+    for item in arr2:
+        result =  item + " " + str(len(item) )
+        arr.append( result)
+    return arr
+    #your code here
