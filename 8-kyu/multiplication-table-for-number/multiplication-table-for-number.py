106fccbe4a26ede981871f7a3d084cafd7a26c6a
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-06-10 14:50:46 +0530

    Add solution - CodeHub

---
+++ 8-kyu/multiplication-table-for-number/multiplication-table-for-number.py
@@ -0,0 +1,6 @@
+def multi_table(number):
+    answer = ''
+    for i in range(1, 11):  # Standard 1 to 10 table
+        x = f'{i} * {number} = {i * number}\n'
+        answer += x
+    return answer.strip()
