4b517c748e6450c4dac3a2ceeeba507a3f3215c4
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-06-16 19:36:42 +0530

    Add solution - CodeHub

---
+++ 8-kyu/quarter-of-the-year/quarter-of-the-year.py
@@ -0,0 +1,12 @@
+ 
+def quarter_of(month):
+    # your code here
+    if month >=10:
+        return 4
+    elif month >= 7:
+        return 3
+    elif month >= 4:
+        return 2
+    elif month >= 1:
+        return 1
+        
