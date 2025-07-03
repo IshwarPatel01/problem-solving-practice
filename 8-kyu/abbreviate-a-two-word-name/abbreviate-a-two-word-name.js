368766e4892ba138570ff61bf29087fe688f3c2e
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-07-03 18:06:34 +0530

    Add solution - CodeHub

---
+++ 8-kyu/abbreviate-a-two-word-name/abbreviate-a-two-word-name.js
@@ -0,0 +1,9 @@
+function abbrevName(name){
+  let split = name.split(" ")
+  
+  let first = split[0].charAt(0).toUpperCase()
+  let second = split[1].charAt(0).toUpperCase()
+  
+  return `${first}.${second}`
+  
+} 
