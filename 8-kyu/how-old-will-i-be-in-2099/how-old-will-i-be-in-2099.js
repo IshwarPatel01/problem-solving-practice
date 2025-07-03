eaf999b385e50626a55f81ae6f718bcfb613b099
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-07-03 17:56:16 +0530

    Add solution - CodeHub

---
+++ 8-kyu/how-old-will-i-be-in-2099/how-old-will-i-be-in-2099.js
@@ -0,0 +1,10 @@
+ 
+function  calculateAge(b,y) {  
+  if (b - y === 1) return `You will be born in 1 year.`
+  if(y-b === 1) return `You are 1 year old.`
+  if(b > y) return `You will be born in ${b- y} years.`
+  if(y > b) return `You are ${y-b} years old.`
+  if(y === b) return `You were born this very year!`
+}
+​
+​
