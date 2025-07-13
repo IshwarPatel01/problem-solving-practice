a643c6cd047dbe4b3f75f99095fc90dbb8b9669c
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-07-13 10:58:44 +0530

    Update solution - CodeHub

--- 8-kyu/grasshopper-array-mean/grasshopper-array-mean.js
+++ 8-kyu/grasshopper-array-mean/grasshopper-array-mean.js
@@ -1,9 +1,4 @@
  
 function findAverage(nums) {
-  return nums.reduce((acc,curr) => acc + curr,0) / nums.length
-   
-  }
-​
-// var findAverage = function(nums){
-//   return nums.reduce((a, b) => a + b, 0) / nums.length;
-// }
+  return nums.reduce((a,b) => a + b) / nums.length
+}
